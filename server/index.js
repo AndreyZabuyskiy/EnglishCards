aca682fb34c5ff8c02a98fdecc629f003a129de6
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-07-02 22:17:48 +0300

    Added routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const router = require('./routes/index');
 
 
 const PORT = config.get('port') || 5000;
@@ -9,6 +10,7 @@ const PORT = config.get('port') || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 async function start(){
   try{
