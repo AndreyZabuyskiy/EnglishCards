0ca7acaf6b9917d16e0aedb93944f029dea503a2
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2022-07-17 23:17:59 +0300

    Layout module component

--- client/src/pages/Module/Module.js
+++ client/src/pages/Module/Module.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchModuleBy } from "../../redux/actions";
@@ -6,6 +6,8 @@ import style from './Module.module.scss';
 import { CardElement } from "../../components/CardElement";
 
 export const Module = () => {
+  const [cardItemIndex, setCardItemIndex] = useState(0);
+
   const { id } = useParams();
   const dispatch = useDispatch();
 
@@ -17,15 +19,48 @@ export const Module = () => {
     const { moduleReducer } = state;
     return moduleReducer.module;
   });
-
-  console.log(moduleData);
   
+  const clickBack = () => {
+    if(cardItemIndex > 0) { 
+      setCardItemIndex(cardItemIndex - 1);
+    }
+  }
+
+  const clickForward = () => {
+    if(cardItemIndex < moduleData.words.length - 1) {
+      setCardItemIndex(cardItemIndex + 1);
+    }
+  }
+
   return (
     <div className={style.container}>
       <div className={style.title}> { moduleData?.module?.title } </div>
-      <div className={style.cards}>
-        {moduleData?.words.map(card => {
+
+      <div className={style.header}>
+        <div className={style.UIRow}>
+          <div>Карточки</div>
+          <div>Заучивание</div>
+          <div>Письмо</div>
+          <div>Правописание</div>
+          <div>Тест</div>
+        </div>
+        
+        <div className={style.card__item__container}>
+          <div className={style.card__item}>
+            { moduleData?.words?.[cardItemIndex].value }
+          </div>
+          <div className={style.cards__items__buttons}>
+            <button onClick={() => clickBack()}>Назад</button>
+            <div className={ style.counter__cards } >{ cardItemIndex }</div>
+            <button onClick={() => clickForward()}>Вперед</button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        {moduleData?.words.map((card, index) => {
           return <CardElement
+            key={index}
             value={card.value}
             translate={card.translate} />
         })}
@@ -33,12 +68,3 @@ export const Module = () => {
     </div>
   )
 }
-
-/*
-return (
-  <div className={style.card}>
-    <div>{card.value}</div>
-    <div className={style.card__translate}>{card.translate}</div>
-  </div>
-)
-*/
