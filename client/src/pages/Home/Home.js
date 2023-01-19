b4786932a1532b3bbba13d5a79b7848ae402c0b1
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2023-01-19 23:41:55 +0200

    Layout filter in Home component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { StudyModule, Navbar } from "../../components";
 import { fetchModules } from "../../redux/actions";
@@ -6,6 +6,9 @@ import style from './Home.module.css';
 
 export const Home = () => {
   const dispatch = useDispatch();
+
+  const [isShowOptions, setIsShowOptions] = useState(false);
+  const [option, setOption] = useState('Created');
 
   const user = useSelector(state => {
     const { authReducer } = state;
@@ -21,13 +24,38 @@ export const Home = () => {
     dispatch(fetchModules());
   }, []);
 
+  const onClickForm = () => {
+    setIsShowOptions(false);
+  }
+
+  const onClickSelect = (e) => {
+    e.stopPropagation();
+    setIsShowOptions(prev => !prev);
+  }
+
+  const onClickOption = (opt) => {
+    setOption(opt);
+  }
+
   return (
-    <div className={style.container}>
+    <div className={style.container} onClick={onClickForm}>
       <div className={style.container__navbar}>
         <Navbar />
       </div>
 
       <div className={style.content}>
+        <div className={style.filter__inputs}>
+          <div className={style.filter}>
+            <div className={style.select__filter} onClick={onClickSelect}>{ option }</div>
+            <div className={style.options} style={{display: isShowOptions ? 'block' : 'none'}}>
+              <div className={style.option} onClick={() => onClickOption('Created')}>Created</div>
+              <div className={style.option} onClick={() => onClickOption('Viewed')}>Viewed</div>
+            </div>
+          </div>
+
+          <div><input type="text" placeholder="search modules..." /></div>
+        </div>
+
         {modules?.map((item, index) => (
           <StudyModule
             key={index}
