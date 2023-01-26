9abe5ba30b6aaed473608cceeb5d359e453c6901
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2023-01-26 23:01:41 +0200

    Added helpers folder

---
+++ server/helpers/countGroupsByModuleSize.js
@@ -0,0 +1,182 @@
+export const countGroupsByModuleSize = [
+    {
+      count: 2,
+      cards: {
+        TrueOrFalseCard: 0,
+        TestCard: 1,
+        JoinCards: 0,
+        WriteCards: 1
+      }
+    },
+    {
+      count: 3,
+      cards: {
+        TrueOrFalseCard: 1,
+        TestCard: 1,
+        JoinCards: 0,
+        WriteCards: 1
+      }
+    },
+    {
+      count: 4,
+      cards: {
+        TrueOrFalseCard: 2,
+        TestCard: 1,
+        JoinCards: 0,
+        WriteCards: 1
+      }
+    },
+    {
+      count: 5,
+      cards: {
+        TrueOrFalseCard: 2,
+        TestCard: 2,
+        JoinCards: 0,
+        WriteCards: 1
+      }
+    },
+    {
+      count: 6,
+      cards: {
+        TrueOrFalseCard: 2,
+        TestCard: 2,
+        JoinCards: 0,
+        WriteCards: 2
+      }
+    },
+    {
+      count: 7,
+      cards: {
+        TrueOrFalseCard: 3,
+        TestCard: 2,
+        JoinCards: 0,
+        WriteCards: 2
+      }
+    },
+    {
+      count: 8,
+      cards: {
+        TrueOrFalseCard: 2,
+        TestCard: 2,
+        JoinCards: 2,
+        WriteCards: 2
+      }
+    },
+    {
+      count: 9,
+      cards: {
+        TrueOrFalseCard: 2,
+        TestCard: 3,
+        JoinCards: 2,
+        WriteCards: 2
+      }
+    },
+    {
+      count: 10,
+      cards: {
+        TrueOrFalseCard: 2,
+        TestCard: 3,
+        JoinCards: 2,
+        WriteCards: 3
+      }
+    },
+    {
+      count: 11,
+      cards: {
+        TrueOrFalseCard: 3,
+        TestCard: 3,
+        JoinCards: 2,
+        WriteCards: 3
+      }
+    },
+    {
+      count: 12,
+      cards: {
+        TrueOrFalseCard: 3,
+        TestCard: 3,
+        JoinCards: 3,
+        WriteCards: 3
+      }
+    },
+    {
+      count: 13,
+      cards: {
+        TrueOrFalseCard: 3,
+        TestCard: 4,
+        JoinCards: 3,
+        WriteCards: 3
+      }
+    },
+    {
+      count: 14,
+      cards: {
+        TrueOrFalseCard: 3,
+        TestCard: 4,
+        JoinCards: 3,
+        WriteCards: 4
+      }
+    },
+    {
+      count: 15,
+      cards: {
+        TrueOrFalseCard: 4,
+        TestCard: 4,
+        JoinCards: 3,
+        WriteCards: 4
+      }
+    },
+    {
+      count: 15,
+      cards: {
+        TrueOrFalseCard: 4,
+        TestCard: 4,
+        JoinCards: 3,
+        WriteCards: 4
+      }
+    },
+    {
+      count: 16,
+      cards: {
+        TrueOrFalseCard: 4,
+        TestCard: 4,
+        JoinCards: 4,
+        WriteCards: 4
+      }
+    },
+    {
+      count: 17,
+      cards: {
+        TrueOrFalseCard: 4,
+        TestCard: 5,
+        JoinCards: 4,
+        WriteCards: 4
+      }
+    },
+    {
+      count: 18,
+      cards: {
+        TrueOrFalseCard: 4,
+        TestCard: 5,
+        JoinCards: 4,
+        WriteCards: 5
+      }
+    },
+    {
+      count: 19,
+      cards: {
+        TrueOrFalseCard: 5,
+        TestCard: 5,
+        JoinCards: 4,
+        WriteCards: 5
+      }
+    },
+    {
+      count: 20,
+      cards: {
+        TrueOrFalseCard: 5,
+        TestCard: 5,
+        JoinCards: 5,
+        WriteCards: 5
+      }
+    },
+];
--- server/services/testModuleService.js
+++ server/services/testModuleService.js
@@ -1,5 +1,24 @@
+import StudyModule from "../models/StudyModule.js";
+import Card from '../models/Card.js';
+import { countGroupsByModuleSize } from '../helpers/countGroupsByModuleSize.js';
+
+
 class TestModuleService {
-  async getTestModule (userId, moduleId) {
+  async getTestModule(userId, moduleId) {
+    const module = await StudyModule.findById(moduleId);
+    const cards = await Card.find({ module: moduleId });
+
+    const testModule = {
+      title: module.title,
+      countCards: cards.length,
+      groups: {
+        trueOrFalseCards: [],
+        testCards: [],
+        joinCards: [],
+        writeCards: []
+      }
+    }
+    
     return 'getTestModule';
   }
 }
