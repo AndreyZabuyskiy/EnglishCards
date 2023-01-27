100d079f9c69a05b4de4bfbc5c29bcd7440ec61e
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2023-01-27 14:47:53 +0200

    Added getTestModule endpoint

--- server/helpers/countGroupsByModuleSize.js
+++ server/helpers/countGroupsByModuleSize.js
@@ -1,182 +1,116 @@
-export const countGroupsByModuleSize = [
-    {
-      count: 2,
-      cards: {
-        TrueOrFalseCard: 0,
-        TestCard: 1,
-        JoinCards: 0,
-        WriteCards: 1
-      }
-    },
-    {
-      count: 3,
-      cards: {
-        TrueOrFalseCard: 1,
-        TestCard: 1,
-        JoinCards: 0,
-        WriteCards: 1
-      }
-    },
-    {
-      count: 4,
-      cards: {
-        TrueOrFalseCard: 2,
-        TestCard: 1,
-        JoinCards: 0,
-        WriteCards: 1
-      }
-    },
-    {
-      count: 5,
-      cards: {
-        TrueOrFalseCard: 2,
-        TestCard: 2,
-        JoinCards: 0,
-        WriteCards: 1
-      }
-    },
-    {
-      count: 6,
-      cards: {
-        TrueOrFalseCard: 2,
-        TestCard: 2,
-        JoinCards: 0,
-        WriteCards: 2
-      }
-    },
-    {
-      count: 7,
-      cards: {
-        TrueOrFalseCard: 3,
-        TestCard: 2,
-        JoinCards: 0,
-        WriteCards: 2
-      }
-    },
-    {
-      count: 8,
-      cards: {
-        TrueOrFalseCard: 2,
-        TestCard: 2,
-        JoinCards: 2,
-        WriteCards: 2
-      }
-    },
-    {
-      count: 9,
-      cards: {
-        TrueOrFalseCard: 2,
-        TestCard: 3,
-        JoinCards: 2,
-        WriteCards: 2
-      }
-    },
-    {
-      count: 10,
-      cards: {
-        TrueOrFalseCard: 2,
-        TestCard: 3,
-        JoinCards: 2,
-        WriteCards: 3
-      }
-    },
-    {
-      count: 11,
-      cards: {
-        TrueOrFalseCard: 3,
-        TestCard: 3,
-        JoinCards: 2,
-        WriteCards: 3
-      }
-    },
-    {
-      count: 12,
-      cards: {
-        TrueOrFalseCard: 3,
-        TestCard: 3,
-        JoinCards: 3,
-        WriteCards: 3
-      }
-    },
-    {
-      count: 13,
-      cards: {
-        TrueOrFalseCard: 3,
-        TestCard: 4,
-        JoinCards: 3,
-        WriteCards: 3
-      }
-    },
-    {
-      count: 14,
-      cards: {
-        TrueOrFalseCard: 3,
-        TestCard: 4,
-        JoinCards: 3,
-        WriteCards: 4
-      }
-    },
-    {
-      count: 15,
-      cards: {
-        TrueOrFalseCard: 4,
-        TestCard: 4,
-        JoinCards: 3,
-        WriteCards: 4
-      }
-    },
-    {
-      count: 15,
-      cards: {
-        TrueOrFalseCard: 4,
-        TestCard: 4,
-        JoinCards: 3,
-        WriteCards: 4
-      }
-    },
-    {
-      count: 16,
-      cards: {
-        TrueOrFalseCard: 4,
-        TestCard: 4,
-        JoinCards: 4,
-        WriteCards: 4
-      }
-    },
-    {
-      count: 17,
-      cards: {
-        TrueOrFalseCard: 4,
-        TestCard: 5,
-        JoinCards: 4,
-        WriteCards: 4
-      }
-    },
-    {
-      count: 18,
-      cards: {
-        TrueOrFalseCard: 4,
-        TestCard: 5,
-        JoinCards: 4,
-        WriteCards: 5
-      }
-    },
-    {
-      count: 19,
-      cards: {
-        TrueOrFalseCard: 5,
-        TestCard: 5,
-        JoinCards: 4,
-        WriteCards: 5
-      }
-    },
-    {
-      count: 20,
-      cards: {
-        TrueOrFalseCard: 5,
-        TestCard: 5,
-        JoinCards: 5,
-        WriteCards: 5
-      }
-    },
-];
+export const countGroupsByModuleSize = {
+  2: {
+    TrueOrFalseCard: 0,
+    TestCards: 1,
+    JoinCards: 0,
+    WriteCards: 1
+  },
+  3: {
+    TrueOrFalseCard: 1,
+    TestCards: 1,
+    JoinCards: 0,
+    WriteCards: 1
+  },
+  4: {
+    TrueOrFalseCard: 2,
+    TestCards: 1,
+    JoinCards: 0,
+    WriteCards: 1
+  },
+  5: {
+    TrueOrFalseCard: 2,
+    TestCards: 2,
+    JoinCards: 0,
+    WriteCards: 1
+  },
+  6: {
+      TrueOrFalseCard: 2,
+      TestCards: 2,
+      JoinCards: 0,
+      WriteCards: 2
+  },
+  7: {
+    TrueOrFalseCard: 3,
+    TestCards: 2,
+    JoinCards: 0,
+    WriteCards: 2
+  },
+  8: {
+    TrueOrFalseCard: 2,
+    TestCards: 2,
+    JoinCards: 2,
+    WriteCards: 2
+  },
+  9: {
+    TrueOrFalseCard: 2,
+    TestCards: 3,
+    JoinCards: 2,
+    WriteCards: 2
+  },
+  10: {
+    TrueOrFalseCard: 2,
+    TestCards: 3,
+    JoinCards: 2,
+    WriteCards: 3
+  },
+  11: {
+    TrueOrFalseCard: 3,
+    TestCards: 3,
+    JoinCards: 2,
+    WriteCards: 3
+  },
+  12: {
+    TrueOrFalseCard: 3,
+    TestCards: 3,
+    JoinCards: 3,
+    WriteCards: 3
+  },
+  13: {
+    TrueOrFalseCard: 3,
+    TestCards: 4,
+    JoinCards: 3,
+    WriteCards: 3
+  },
+  14: {
+    TrueOrFalseCard: 3,
+    TestCards: 4,
+    JoinCards: 3,
+    WriteCards: 4
+  },
+  15: {
+    TrueOrFalseCard: 4,
+    TestCards: 4,
+    JoinCards: 3,
+    WriteCards: 4
+  },
+  16: {
+    TrueOrFalseCard: 4,
+    TestCards: 4,
+    JoinCards: 4,
+    WriteCards: 4
+  },
+  17: {
+    TrueOrFalseCard: 4,
+    TestCards: 5,
+    JoinCards: 4,
+    WriteCards: 4
+  },
+  18: {
+    TrueOrFalseCard: 4,
+    TestCards: 5,
+    JoinCards: 4,
+    WriteCards: 5
+  },
+  19: {
+    TrueOrFalseCard: 5,
+    TestCards: 5,
+    JoinCards: 4,
+    WriteCards: 5
+  },
+  20: {
+    TrueOrFalseCard: 5,
+    TestCards: 5,
+    JoinCards: 5,
+    WriteCards: 5
+  }
+};
--- server/services/testModuleService.js
+++ server/services/testModuleService.js
@@ -2,12 +2,11 @@ import StudyModule from "../models/StudyModule.js";
 import Card from '../models/Card.js';
 import { countGroupsByModuleSize } from '../helpers/countGroupsByModuleSize.js';
 
-
 class TestModuleService {
   async getTestModule(userId, moduleId) {
     const module = await StudyModule.findById(moduleId);
     const cards = await Card.find({ module: moduleId });
-
+    
     const testModule = {
       title: module.title,
       countCards: cards.length,
@@ -18,8 +17,124 @@ class TestModuleService {
         writeCards: []
       }
     }
-    
-    return 'getTestModule';
+
+    let countGroups = null;
+
+    if (cards.length < 20) {
+      countGroups = countGroupsByModuleSize[cards.length];
+    } else {
+      countGroups = countGroupsByModuleSize[20];
+    }
+
+    const freeCards = cards.slice(0);
+    let randomNumberPositionCard = null;
+
+    if (countGroups.TrueOrFalseCard > 0) {
+      const trueOrFalseCards = [];
+
+      for (let i = 0; i < countGroups.TrueOrFalseCard; ++i) {
+        randomNumberPositionCard = this.getRandomNumber(0, freeCards.length);
+        const card = freeCards[randomNumberPositionCard];
+        freeCards.splice(randomNumberPositionCard, 1);
+
+        const isTrueOrFalseRandom = this.getRandomNumber(0, 2);
+        let value = '';
+
+        if (isTrueOrFalseRandom) {
+          value = card.value;
+        } else {
+          randomNumberPositionCard = this.getRandomNumber(0, freeCards.length);
+          const rndCard = freeCards[randomNumberPositionCard];
+          value = rndCard.value;
+        }
+
+        trueOrFalseCards.push({
+          cardId: card._id,
+          translate: card.translate,
+          value: value,
+          pathToFile: card.pathToFile,
+          urlToImage: card.urlToImage
+        });
+
+        testModule.groups.trueOrFalseCards = trueOrFalseCards;
+      }
+    }
+
+    if (countGroups.TestCards > 0) {
+      const testCards = [];
+
+      for (let i = 0; i < countGroups.TestCards; ++i) {
+        randomNumberPositionCard = this.getRandomNumber(0, freeCards.length);
+        const card = freeCards[randomNumberPositionCard];
+        freeCards.splice(randomNumberPositionCard, 1);
+
+        const optionsCard = [card.value];
+        for (let j = 0; j < 3; ++j) {
+          randomNumberPositionCard = this.getRandomNumber(0, freeCards.length);
+          const rndCard = freeCards[randomNumberPositionCard];
+          optionsCard.push(rndCard.value);
+        }
+
+        testCards.push({
+          cardId: card._id,
+          translate: card.translate,
+          pathToFile: card.pathToFile,
+          urlToImage: card.urlToImage,
+          options: optionsCard
+        });
+
+        testModule.groups.testCards = testCards;
+      }
+    }
+
+    if (countGroups.JoinCards > 0) {
+      const cards = [];
+      const values = [];
+
+      for (let i = 0; i < countGroups.JoinCards; ++i) {
+        randomNumberPositionCard = this.getRandomNumber(0, freeCards.length);
+        const card = freeCards[randomNumberPositionCard];
+        freeCards.splice(randomNumberPositionCard, 1);
+
+        cards.push({
+          cardId: card._id,
+          translate: card.translate,
+          pathToFile: card.pathToFile,
+          urlToImage: card.urlToImage
+        });
+
+        values.push(card.value);
+      }
+      
+      testModule.groups.joinCards = {
+        cards, values
+      };
+    }
+
+    if (countGroups.WriteCards > 0) {
+      const writeCards = [];
+
+      for (let i = 0; i < countGroups.WriteCards; ++i) {
+        randomNumberPositionCard = this.getRandomNumber(0, freeCards.length);
+        const card = freeCards[randomNumberPositionCard];
+        freeCards.splice(randomNumberPositionCard, 1);
+
+        writeCards.push({
+          cardId: card._id,
+          translate: card.translate,
+          pathToFile: card.pathToFile,
+          urlToImage: card.urlToImage
+        });
+
+        testModule.groups.writeCards = writeCards;
+      }
+    }
+
+    return testModule;
+  }
+
+  getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
   }
 }
 
