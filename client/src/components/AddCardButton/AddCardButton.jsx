86b6c46284b924fd0e4953089d381d0ce955dcc4
AndreyZabuyskiy <zabuyskiy84@gmail.com>
2023-02-14 19:32:30 +0200

    Refactoring CardForm

--- client/src/components/AddCardButton/AddCardButton.jsx
+++ client/src/components/AddCardButton/AddCardButton.jsx
@@ -1,31 +1,29 @@
 import style from './AddCardButton.module.css';
+import React from 'react';
 
-export const AddCardButton = (props) => {
-  const clickAddCardButton = e => {
-    const changedCards = [];
+export const AddCardButton = React.memo(({ index, setCards }) => {
+  const clickAddCardButton = () => {
+    setCards(prev => {
+      const cards = prev.slice(0);
+      cards.push({
+        _id: cards.length,
+        value: '',
+        translate: '',
+        isViewUploadImage: false,
+        searchQuery: '',
+        pathToFile: '',
+        isUrlImage: false,
+        urlToImage: ''
+      });
 
-    props.cards.forEach(card => {
-      changedCards.push(card);
+      return cards;
     });
-
-    changedCards.push({
-      _id: props.cards.length,
-      value: '',
-      translate: '',
-      isViewUploadImage: false,
-      searchQuery: '',
-      pathToFile: '',
-      isUrlImage: false,
-      urlToImage: ''
-    });
-
-    props.setCards(changedCards);
   }
 
   return (
     <button className={style.add__card} onClick={clickAddCardButton}>
-      <div className={style.index}>{props.index + 1}</div>
+      <div className={style.index}>{index + 1}</div>
       <span className={style.content}> + Add card </span>
     </button>
   )
-};
+});
--- client/src/components/CardForm/CardForm.jsx
+++ client/src/components/CardForm/CardForm.jsx
@@ -1,15 +1,15 @@
 import style from './CardForm.module.css';
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { REACT_APP_API_URL } from '../../http/baseUrl';
-import { useSelector } from 'react-redux';
-import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { uploadFileApi, removeFileApi } from '../../http/moduleApi';
+import { clearImages, fetchImages } from '../../redux/actions/moduleFormAction';
 
-export const CardForm = ({ index, _id, value, translate, pathToFile, searchQuery, cards,
-  urlToImage, handleChangeValue, handleChangeTranslate, handleDeleteCard, handleChangeFile,
-  handleRemoveFile, uploadImage, handleChangeSearchQuery, selectImage, fetchImagesCard }) => {
-  const card = cards.find(card => _id === card._id);
+export const CardForm = React.memo(({ index, _id, value, translate, pathToFile, searchQuery, urlToImage,
+  isViewUploadImage, setCards }) => {
+  const dispatch = useDispatch();
   const inputFileRef = useRef(null);
-  const [searchQueryError, setSearchQueryError] = useState(card.value);
+  const [searchQueryError, setSearchQueryError] = useState(value);
 
   const user = useSelector(state => {
     const { authReducer } = state;
@@ -22,7 +22,7 @@ export const CardForm = ({ index, _id, value, translate, pathToFile, searchQuery
   });
 
   let _images = [];
-  if (images.length) {    
+  if (images.length) {
     for (let i = 0; i < 4; ++i) {
       _images.push(images[i]);
     }
@@ -41,15 +41,195 @@ export const CardForm = ({ index, _id, value, translate, pathToFile, searchQuery
     };
   }
 
-  const onClickUploadImage = e => {
-    uploadImage(_id);
-    setSearchQueryError(card.value);
+  const handleChangeValue = (e) => {
+    setCards(prev => {
+      const indexCard = prev.findIndex(card => card._id === _id);
+      const card = prev[indexCard];
+
+      const updatedCard = {
+        ...card,
+        value: e.target.value
+      }
+
+      const cardsOne = prev.slice(0, indexCard);
+      const cardsTwo = prev.slice(indexCard + 1);
+      const newCards = [...cardsOne, updatedCard, ...cardsTwo];
+
+      return newCards;
+    })
+  }
+
+  const handleChangeTranslate = (e) => {
+    setCards(prev => {
+      const indexCard = prev.findIndex(card => card._id === _id);
+      const card = prev[indexCard];
+
+      const updatedCard = {
+        ...card,
+        translate: e.target.value
+      }
+
+      const cardsOne = prev.slice(0, indexCard);
+      const cardsTwo = prev.slice(indexCard + 1);
+      const newCards = [...cardsOne, updatedCard, ...cardsTwo];
+
+      return newCards;
+    });
+  }
+
+  const handleDeleteCard = () => {
+    setCards(prev => {
+      const indexCard = prev.findIndex(card => card._id === _id);
+
+      const cardsOne = prev.slice(0, indexCard);
+      const cardsTwo = prev.slice(indexCard + 1);
+      const newCards = [...cardsOne, ...cardsTwo];
+
+      return newCards;
+    });
+  }
+
+  const handleChangeFile = async (e) => {
+    try {
+      const data = await uploadFileApi(e.target.files[0]);
+      setCards(prev => {
+        const indexCard = prev.findIndex(card => card._id === _id);
+        const card = prev[indexCard];
+
+        const updatedCard = {
+          ...card,
+          pathToFile: data,
+          isUrlImage: false,
+          urlToImage: ''
+        }
+
+        const cardsOne = prev.slice(0, indexCard);
+        const cardsTwo = prev.slice(indexCard + 1);
+        const newCards = [...cardsOne, updatedCard, ...cardsTwo];
+
+        return newCards;
+      });
+    } catch (err) {
+      console.warn(err);
+      alert('File upload error!');
+    }
+  }
+
+  const handleRemoveFile = async () => {
+    try {
+      if (pathToFile) {
+        await removeFileApi(pathToFile);
+      }
+
+      setCards(prev => {
+        const indexCard = prev.findIndex(card => card._id === _id);
+        const card = prev[indexCard];
+
+        const updatedCard = {
+          ...card,
+          pathToFile: '',
+          isUrlImage: false,
+          urlToImage: ''
+        }
+
+        const cardsOne = prev.slice(0, indexCard);
+        const cardsTwo = prev.slice(indexCard + 1);
+        const newCards = [...cardsOne, updatedCard, ...cardsTwo];
+
+        return newCards;
+      });
+    } catch (err) {
+      console.warn(err);
+      alert('Error when deleting this file');
+    }
+  }
+
+  const handleChangeSearchQuery = (e) => {
+    setCards(prev => {
+      const indexCard = prev.findIndex(card => card._id === _id);
+      const card = prev[indexCard];
+
+      const updatedCard = {
+        ...card,
+        searchQuery: e.target.value
+      }
+
+      const cardsOne = prev.slice(0, indexCard);
+      const cardsTwo = prev.slice(indexCard + 1);
+      const newCards = [...cardsOne, updatedCard, ...cardsTwo];
+      
+      return newCards;
+    });
+  }
+
+  const selectImage = (_urlToImage) => {
+    setCards(prev => {
+      const changedCards = [];
+    
+      prev.forEach(card => {
+        if (_id === card._id) {
+          changedCards.push({
+            ...card,
+            isViewUploadImage: !card.isViewUploadImage,
+            searchQuery: card.value,
+            pathToFile: '',
+            isUrlImage: true,
+            urlToImage: _urlToImage
+          });
+        } else {
+          changedCards.push({
+            ...card,
+            isViewUploadImage: false,
+            searchQuery: card.value,
+            pathToFile: ''
+          });
+        }
+      });
+
+      return changedCards;
+    });
+  }
+
+  const fetchImagesCard = (e) => dispatch(fetchImages(e.target.value));
+
+  const onClickUploadImage = () => {
+    setCards(prev => {
+      const cardIndex = prev.findIndex(card => _id === card._id);
+      const card = prev[cardIndex];
+      
+      if (!card.isViewUploadImage) {
+        dispatch(fetchImages(card.value));
+      } else {
+        dispatch(clearImages());
+      }
+
+      const changedCards = [];
+      prev.forEach(card => {
+        if (_id === card._id) {
+          changedCards.push({
+            ...card,
+            isViewUploadImage: !card.isViewUploadImage,
+            searchQuery: card.value
+          });
+        } else {
+          changedCards.push({
+            ...card,
+            isViewUploadImage: false,
+            searchQuery: card.value
+          });
+        }
+      });
+
+      return changedCards;
+    });
+
+    setSearchQueryError(value);
   }
 
   const onClickEnter = (e) => {
-    if(e.key === 'Enter') {
+    if (e.key === 'Enter') {
       fetchImagesCard(e);
-      setSearchQueryError(card.searchQuery);
+      setSearchQueryError(searchQuery);
     }
   }
 
@@ -61,83 +241,83 @@ export const CardForm = ({ index, _id, value, translate, pathToFile, searchQuery
             <div className={style.index}>{index + 1}</div>
             <div>
               <span>═</span>
-              <button onClick={() => handleDeleteCard(_id)}>🗑</button>
+              <button onClick={handleDeleteCard}>🗑</button>
             </div>
           </div>
         </div>
         <div className={style.inputs}>
           <div className={style.input__value}>
             <input type="text" value={value}
-              onChange={(e) => handleChangeValue(e, _id)} />
-            <p>Термин</p>
+              onChange={handleChangeValue} />
+            <p>Term</p>
           </div>
           <div className={style.input__translate}>
             <input type="text" value={translate}
-              onChange={(e) => handleChangeTranslate(e, _id)} />
-            <p>Определение</p>
+              onChange={handleChangeTranslate} />
+            <p>Definition</p>
           </div>
           {pathToFile || urlToImage
-          ?
+            ?
             <div className={style.image__container} style={backgroundImage}>
-              <button className={style.delete__img} onClick={() => handleRemoveFile(_id)}>
+              <button className={style.delete__img} onClick={handleRemoveFile}>
                 🗑
-              </button>                  
+              </button>
             </div>
-          :
+            :
             <div onClick={onClickUploadImage} className={style.add__image}>
               <label>
                 <div className={style.icon__img}>🖼</div>
-                <span>Изображение</span>
+                <span>Image</span>
               </label>
             </div>
           }
         </div>
       </div>
-      {card.isViewUploadImage &&
-      <div className={style.UploadImages}>
-        <div className={style.UploadImages__header}>
+      {isViewUploadImage &&
+        <div className={style.UploadImages}>
+          <div className={style.UploadImages__header}>
             <input id="search" className={style.text__input}
               type="text" placeholder='Search by images'
               value={searchQuery} onChange={handleChangeSearchQuery}
               onKeyDown={onClickEnter} />
             
-          <div className={style.upload__your__image}>
-            <input type='file' id={`file__${_id}`}
-              ref={inputFileRef} accept='image/*' onChange={(e) => handleChangeFile(e, _id)} />
-            <label htmlFor={`file__${_id}`}>
-              <div>Or upload your image</div>
-            </label>
-          </div>  
-        </div>
+            <div className={style.upload__your__image}>
+              <input type='file' id={`file__${_id}`}
+                ref={inputFileRef} accept='image/*' onChange={handleChangeFile} />
+              <label htmlFor={`file__${_id}`}>
+                <div>Or upload your image</div>
+              </label>
+            </div>
+          </div>
 
-        {card.searchQuery !== ""
-        ?
-          _images.length !== 0
-          ?
-            <div className={style.ImageCarousel}>
-              <div>
-                <button className={style.UIButton__wrapper}> ← </button>
-              </div>
-              {_images.map(img => (
+          {searchQuery !== ""
+            ?
+            _images.length !== 0
+              ?
+              <div className={style.ImageCarousel}>
                 <div>
-                  <button className={style.button__img} onClick={() => selectImage(img, _id)}>
-                    <img src={img} alt="_image" />
-                  </button>
+                  <button className={style.UIButton__wrapper}> ← </button>
                 </div>
-              ))}
-              <div>
-                <button className={style.UIButton__wrapper}> → </button>
+                {_images.map((img, index) => (
+                  <div key={index}>
+                    <button className={style.button__img} onClick={() => selectImage(img)}>
+                      <img src={img} alt="_image" />
+                    </button>
+                  </div>
+                ))}
+                <div>
+                  <button className={style.UIButton__wrapper}> → </button>
+                </div>
               </div>
-            </div>
-          :
-            <div className={style.ImageCarousel}>
-              <p>Don't find images for "{searchQueryError}"</p>
-            </div>
-        :
-          <div className={style.ImageCarousel}></div>
-        }
-      </div>
+              :
+              <div className={style.ImageCarousel}>
+                <p>Don't find images for "{searchQueryError}"</p>
+              </div>
+            :
+            <div className={style.ImageCarousel}></div>
+          }
+        </div>
       }
     </div>
   );
-}
+});
